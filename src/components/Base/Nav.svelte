95fc5821af2644fab0873ebf7e0634685bcaea39
Jim Strother <jim.strother@gmail.com>
2021-04-08 21:57:46 -0500

    Start Nav.svelte got started with navigation just before end of night

---
+++ src/components/Base/Nav.svelte
@@ -0,0 +1,16 @@
+<style>
+  .menuToggle {
+    @apply flex flex-row flex-nowrap text-gray-500 justify-items-center items-center mt-1;
+  }
+  p {
+    @apply pr-1;
+  }
+</style>
+
+<nav>
+  <div class="menuToggle">
+    <p>Menu</p>
+    <i class="fas fa-arrow-alt-circle-down"></i>
+    <i class="fas fa-arrow-alt-circle-up"></i>
+  </div>
+</nav>
--- src/components/Layout/Header.svelte
+++ src/components/Layout/Header.svelte
@@ -1,11 +1,19 @@
+<script>
+  import Nav from '../Base/Nav.svelte';
+</script>
+
 <style>
   header {
     @apply flex flex-col flex-nowrap justify-items-center items-center content-center m-auto;
+  }
+  img {
+    @apply border border-black;
   }
 </style>
 
 <header>
   <a href="https://www.natentaylor.com/" >
-    <img src="/img/NateTaylor_marketplaceBanner.jpg" alt="Nate Taylor and Art Thereby" />
+    <img src="/img/NateTaylor_marketplaceBanner.jpg" alt="Nate Taylor and Art Thereby" title="Nate Taylor and Art Thereby" />
   </a>
+  <Nav />
 </header>
